7dce98c6d613475c9987c515e4011b08e87f0245
Nathaniel Hamovitz <25599558+nhamovitz@users.noreply.github.com>
2020-05-21 13:10:35 -0700

    `$PSScriptPath` doesn't work :(

--- run.ps1
+++ run.ps1
@@ -1,5 +1,6 @@
 # Evidentally `&` is not the background-process indicator that it is on Linux; in Powershell, it's the "call operator" (https://stackoverflow.com/questions/2035193/how-to-run-a-powershell-script#comment14052749_2035209)
-& (Join-Path $PSScriptPath "venv/Scripts/Activate.ps1")
-$env:FLASK_APP = "app.py"
+# & (Join-Path $PSScriptPath "venv/Scripts/Activate.ps1")
+& "./venv/Scripts/Activate.ps1"
+$env:FLASK_APP = "i-lost-my"
 $env:FLASK_ENV = "development"
 flask run
