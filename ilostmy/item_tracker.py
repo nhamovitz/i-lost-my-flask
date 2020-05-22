198cd2b4298e0b479462629f01a44f86195665b3
Nathaniel Hamovitz <25599558+nhamovitz@users.noreply.github.com>
2020-05-21 23:59:04 -0700

    add create route

--- ilostmy/item_tracker.py
+++ ilostmy/item_tracker.py
@@ -12,15 +12,59 @@ bp = Blueprint('item_tracker', __name__)
 def index():
     db = get_db()
     items = db.execute(
-        'SELECT id, created, item_type, email, title, body, author, username, last_seen_time, place'
+        'SELECT *'
         'FROM item'
         ' ORDER BY created DESC'
         # ' ORDER BY item_type DESC'
     ).fetchall()
-    if bp.debug:
-        from pprint import pprint as pp
-        print(type(items))
-        print(type(items[0]))
-        pp(items[0])
+    # # if bp.debug:
+    # from pprint import pprint as pp
+    # print(type(items))
+    # print(type(items[0]))
+    # pp(items[0])
 
-    return render_template('blog/index.html', items=items)
+    return render_template('item_tracker/index.html', items=items)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        item_type = request.form['item-type']
+        email = request.form['email']
+        title = request.form['title']
+        body = request.form['body']
+        author = request.form['author']
+        last_seen_time = request.form['last_seen_time']
+        place = request.form['place']
+        errors = []
+
+        if not title:
+            errors.append("Title is required.")
+        elif not item_type:
+            errors.append("Item type is required.")
+        elif not email:
+            errors.append("Email is required.")
+
+        if body == '':
+            body = None
+        if author == '':
+            author = None
+        if last_seen_time == '':
+            last_seen_time = None
+        if place == '':
+            place = None
+
+        if errors is not []:
+            flash(' '.join(errors))
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (created, item_type, email, title, body, author, last_seen_time)'
+                ' VALUES (?, ?, ?)',
+                (created, item_type, email, title, body, author, last_seen_time)
+            )
+            db.commit()
+            # item_id = db.execute('SELECT last_insert_rowid()')
+            return redirect(url_for('item_tracker.index'))
+
+    return render_template('item_tracker/create.html')
